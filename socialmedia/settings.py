661cdd545c4cb865b26826315989c49ea98d0f52
Mani Barathi <manibarathi.s@gmail.com>
2020-10-23 00:09:06 +0530

    static files fixed

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'socialmedia/static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
