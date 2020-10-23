7128a609fd7db1c4b36835de0713933d4ec716b1
Mani Barathi <manibarathi.s@gmail.com>
2020-10-23 12:26:47 +0530

    commited

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
+EMAIL_HOST_USER = 'manibarathi.s@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
